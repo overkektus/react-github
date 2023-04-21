b9a355746b756a4385a701b63707581bd5833f84
Yahor Piskunou <egor.piskunou@gmail.com>
2023-04-21 19:10:28 +0200

    added two pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { HomePage } from "./pages/HomePage";
+import { FavouritesPage } from "./pages/FavoritesPage";
+import { Navigation } from "./components/Navigation";
 
 function App() {
-  return <h1>Hello React</h1>;
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="" element={<HomePage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
