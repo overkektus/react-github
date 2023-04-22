3a5885247732ffa9a8edb00cd638cfab19fc3717
Yahor Piskunou <egor.piskunou@gmail.com>
2023-04-22 12:41:22 +0200

    implement fetch data from github via RTK

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import { useSearchUsersQuery } from "../store/github/github.api";
+import { useDebounce } from "../hooks/debounce";
 
 export function HomePage() {
-  return <div>Home</div>;
+  const [search, setSearch] = useState("");
+  const debounced = useDebounce(search);
+  const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3,
+  });
+
+  useEffect(() => {
+    console.log(debounced);
+  }, [debounced]);
+
+  return (
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      {isError && (
+        <p className="text-center text-red-600">Something went wrong...</p>
+      )}
+      <div className="relative w-[560px]">
+        <input
+          type="text"
+          className="border py-2 px-4 w-full h-[42px] mb-2"
+          placeholder="Search for Github username..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <div className="absolute top-[42px] left-0 right-0 max-h[200px] shadow-md bg-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+          unde.
+        </div>
+      </div>
+    </div>
+  );
 }
