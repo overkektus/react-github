16fd69ea620f403b8405c83d904f33df669b54d8
Yahor Piskunou <egor.piskunou@gmail.com>
2023-04-25 21:18:11 +0200

    fetch and display user repos

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 
-import { useSearchUsersQuery } from "../store/github/github.api";
+import { useSearchUsersQuery, useLazyGetUserReposQuery } from "../store/github/github.api";
 import { useDebounce } from "../hooks/debounce";
+import { RepoCard } from "../components/RepoCard";
 
 export function HomePage() {
   const [search, setSearch] = useState("");
@@ -12,9 +13,16 @@ export function HomePage() {
     refetchOnFocus: true,
   });
 
+  const [fetchRepos, { isLoading: areReposLoading, data: repos }] = useLazyGetUserReposQuery();
+
   useEffect(() => {
     setDropdown(debounced.length > 3 && data?.length! > 0);
   }, [debounced, data]);
+
+  const clickHandler = (username: string) => {
+    fetchRepos(username);
+    setDropdown(false);
+  }
 
   return (
     <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
@@ -36,6 +44,7 @@ export function HomePage() {
             {data?.map((user) => (
               <li
                 key={user.id}
+                onClick={() => clickHandler(user.login)}
                 className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
               >
                 {user.login}
@@ -43,6 +52,10 @@ export function HomePage() {
             ))}
           </ul>
         )}
+        <div className="container">
+          { areReposLoading && <p className="text-center">Repos are loading...</p> }
+          { repos?.map(repo => <RepoCard key={repo.id} repo={repo} />) }
+        </div>
       </div>
     </div>
   );
